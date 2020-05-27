d63c4c5c847f011f1fb683b8ef3f41be97b35052
Veserion <48389713+Veserion@users.noreply.github.com>
2020-05-27 15:37:59 +0300

    refactoring :recycle:

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { autorun } from 'mobx';
 import { RootStore } from './stores';
-import { loadState, saveState } from './utils/localStore';
 import { Provider } from 'mobx-react';
 
-const initState = loadState();
 const mobXStore = new RootStore();
 
 ReactDOM.render(<Provider {...mobXStore}>
